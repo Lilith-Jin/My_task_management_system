c2433c33eabf9ddf764bc4f1027f2394f7ccf953
Lilith <li6253462@gmail.com>
2022-04-25 20:46:25 +0800

    add new,create action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,27 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content, :priority, :status, :start_time, :end_time)
+  end
 end
+
