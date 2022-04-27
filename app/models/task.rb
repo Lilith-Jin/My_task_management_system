621aebdd96c3916e755525a132b77572a090a3af
Lilith <li6253462@gmail.com>
2022-04-27 17:15:40 +0800

    adjust enum and index view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    enum priority:{"高": 1, "中": 2, "低": 3 },_default: "高"
-    enum status:{"待處理": 1, "進行中": 2, "已完成": 3 },_default: "待處理"
+    enum :priority, [:high, :medium, :low] , default: :high
+    enum :status, [:waiting, :running, :done], default: :waiting
 end
