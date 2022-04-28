96d645ae34716422d2d35b4e81524846257a547b
Lilith <li6253462@gmail.com>
2022-04-28 14:13:26 +0800

    refactor the new, edit action test

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -3,7 +3,10 @@ require "rails_helper"
 RSpec.feature "task management", :type => :feature do
   context 'create new task' do
     scenario "Successfuly creates a new task" do
-      visit "/tasks/new"
+      visit tasks_path
+      click_link("新增任務")
+
+      expect(current_path).to have_content('tasks/new')
       within("form") do 
           fill_in 'task_title', with:"task1"
           fill_in 'task_content', with:"123"
@@ -16,14 +19,17 @@ RSpec.feature "task management", :type => :feature do
       click_button "新增任務"
 
       expect(page).to have_text("新增任務成功")
-      # expect(page).to have_field('task_start_time', with: Time.new(2022, 04, 29, 12))
     end
   end
 
   context 'update new task' do
     scenario "Successfuly updates a new task" do
       task = Task.create(title:"task1", content:"123", start_time: DateTime.now, end_time: DateTime.now ,priority:"high", status:"running")
-      visit edit_task_path(task)
+      # visit edit_task_path(task)
+      visit tasks_path
+      click_link("編輯任務")
+
+      expect(current_path).to have_content(edit_task_path(task))
       within("form") do 
           fill_in 'task_title', with:"task1"
           fill_in 'task_content', with:"123"
@@ -39,11 +45,16 @@ RSpec.feature "task management", :type => :feature do
       
     end
   end
+  context 'task index' do
+    scenario "Successfuly delete a task" do
+      task = Task.create(title:"task1", content:"123", start_time: DateTime.now, end_time: DateTime.now ,priority:"high", status:"running")
+      visit tasks_path
+    
+      click_link '刪除任務'
 
-#   scenario "Successfuly delete a task" do
-#     task = Task.create(title:"task1", content:"123", start_time:"202204080800", end_time:"202204080800",priority:"high", status:"running")
-#     visit tasks_path
-#     click_button '刪除任務'
-#     expect(page).to have_content '任務已刪除'
-#   end
+      expect(click_link '刪除任務').to change(Task, :count).by(-1)
+      expect(page).to have_content '任務已刪除'
+    end
+  end
+
 end
