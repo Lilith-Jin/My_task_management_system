d8e5f77164c926739d8704c43ffdb2e9ac20876b
Lilith <li6253462@gmail.com>
2022-04-28 11:50:02 +0800

    pass update task test

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -1,21 +1,43 @@
 require "rails_helper"
 
 RSpec.feature "task management", :type => :feature do
-  scenario "Successfuly creates a new task" do
-    visit "/tasks/new"
-    within("form") do 
-        fill_in 'task_title', with:"task1"
-        fill_in 'task_content', with:"123"
-        fill_in 'task_start_time', with: DateTime.now
-        fill_in 'task_end_time', with: DateTime.now
-        select 'high', from: 'task_priority'
-        select'running', from: 'task_status'
+  context 'create new task' do
+    scenario "Successfuly creates a new task" do
+      visit "/tasks/new"
+      within("form") do 
+          fill_in 'task_title', with:"task1"
+          fill_in 'task_content', with:"123"
+          fill_in 'task_start_time', with: DateTime.now
+          fill_in 'task_end_time', with: DateTime.now
+          select 'high', from: 'task_priority'
+          select'running', from: 'task_status'
+    end
+
+      click_button "新增任務"
+
+      expect(page).to have_text("新增任務成功")
+      # expect(page).to have_field('task_start_time', with: Time.new(2022, 04, 29, 12))
+    end
   end
 
-    click_button "新增任務"
+  context 'update new task' do
+    scenario "Successfuly updates a new task" do
+      task = Task.create(title:"task1", content:"123", start_time: DateTime.now, end_time: DateTime.now ,priority:"high", status:"running")
+      visit edit_task_path(task)
+      within("form") do 
+          fill_in 'task_title', with:"task1"
+          fill_in 'task_content', with:"123"
+          fill_in 'task_start_time', with: DateTime.now
+          fill_in 'task_end_time', with: DateTime.now
+          select 'high', from: 'task_priority'
+          select'running', from: 'task_status'
+    end
 
-    expect(page).to have_text("新增任務成功")
-    # expect(page).to have_field('task_start_time', with: Time.new(2022, 04, 29, 12))
+      click_button "更新任務"
+
+      expect(page).to have_text("更新任務成功")
+      
+    end
   end
 
 #   scenario "Successfuly delete a task" do
