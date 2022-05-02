28c68c0fd6ee6f95f419e401aa3078adccdcfd57
Lilith <li6253462@gmail.com>
2022-05-02 21:11:05 +0800

    rewrite the code v2

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -5,49 +5,48 @@ RSpec.feature "task_management", :type => :feature do
     let!(:task) {FactoryBot.create (:task) }
   
     describe '#create' do
-      it "Successfuly creates a new task " do
-        # p task.title
+      it 'Successfuly creates a new task' do
         visit tasks_path
-        click_link("新增任務")
+        click_link('新增任務')
         expect(current_path).to have_content('tasks/new')
     
-        within("form") do 
-            fill_in 'task_title', with:"task1"
-            fill_in 'task_content', with:"123"
+        within('form') do 
+            fill_in 'task_title', with:'task1'
+            fill_in 'task_content', with:'123'
             fill_in 'task_start_time', with: DateTime.now
             fill_in 'task_end_time', with: DateTime.now
             select 'high', from: 'task_priority'
-            select'running', from: 'task_status'
+            select 'running', from: 'task_status'
         end
 
-        click_button "新增任務"
+        click_button '新增任務'
 
-        expect(page).to have_text("新增任務成功")
+        expect(page).to have_text('新增任務成功')
         expect(current_path).to have_content(tasks_path)
       end
     end
 
     describe '#update' do
-      it "Successfuly updates a new task" do
+      it 'Successfuly updates a new task' do
         visit tasks_path
-        task = Task.find_by(title:'task1')
-        click_link("編輯任務")
+        task = Task.find_by(title: 'task1')
+        click_link('編輯任務')
         expect(current_path).to have_content(edit_task_path(task))
 
-        within("form") do 
-            fill_in 'task_title', with:"task2"
+        within('form') do 
+          fill_in 'task_title', with: 'task2'
         end
 
-        click_button "更新任務"
-        expect(page).to have_text("更新任務成功")
+        click_button '更新任務'
+        expect(page).to have_text('更新任務成功')
         expect(current_path).to have_content(tasks_path)
       end
     end
   
     describe '#show' do
-      it "Successfuly read a task" do
+      it 'Successfuly read a task' do
         visit tasks_path
-        task = Task.find_by(title:'task1')
+        task = Task.find_by(title: 'task1')
         click_link '查看任務'
         expect(current_path).to have_content(task_path(task))
 
@@ -57,15 +56,15 @@ RSpec.feature "task_management", :type => :feature do
     end
   
     describe '#destroy' do
-      it "Successfuly delete a task", driver: :selenium_chrome, js: true  do
+      it 'Successfuly delete a task', driver: :selenium_chrome, js: true do
         visit tasks_path
-        task = Task.find_by(title:'task1')
+        task = Task.find_by(title: 'task1')
+        find(:xpath, "//a[@href='/tasks/#{task.id}']", text: '刪除任務').click
         click_link '刪除任務'
-        accept_alert(text: "您確定要刪除嗎？")
+        accept_alert(text: '您確定要刪除嗎？')
       
         expect(page).to have_current_path(tasks_path)
         expect(page).to have_content('任務已刪除')
-        
       end
     end
   end
