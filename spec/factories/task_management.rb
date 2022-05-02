ab410270a1881d12660eb3f4c7920c1ee0a28e43
Lilith <li6253462@gmail.com>
2022-05-02 18:44:12 +0800

    rewrite the code

--- spec/factories/task_management.rb
+++ spec/factories/task_management.rb
@@ -1,15 +1,10 @@
 FactoryBot.define do
-  factory :task  do
-    title { "task1" }
-    content { "content1" }
-    status { "running" }
-    priority {"high"}
-    start_time {DateTime.now}
-    end_time {DateTime.now + 1.day}
-        
-      # trait :new_task do
-      #   title { 'new_task' }
-      #   content { "new_content" }
-      # end
+  factory :task do
+    title { 'task1' }
+    content { 'content1' }
+    status { 'running' }
+    priority { 'high' }
+    start_time { Time.now }
+    end_time { Time.now + 1.day }
   end
 end
