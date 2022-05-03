a795e791a530461e7b98a7d62b39823fb204ad5d
Lilith <li6253462@gmail.com>
2022-05-03 18:24:42 +0800

    use factory_bot and faker to create a task

---
+++ spec/factories/task.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :task do
+    title { Faker::Name.name }
+    content { Faker::Lorem.paragraph  }
+    status { 'running' }
+    priority { 'high' }
+    start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short)}
+    end_time { Faker::Time.forward(days: 7, format: :short)}
+  end
+end
--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -1,43 +1,64 @@
 require "rails_helper"
 
 RSpec.feature 'task_management', :type => :feature do
-  context 'task CRUD' do
+  context 'with task CRUD' do
     let!(:task) { FactoryBot.create (:task) }
-    it 'Successfuly creates a new task' do
+    it 'Link to creates a new task' do
       visit tasks_path
       click_link('新增任務')
-      expect(current_path).to have_content('tasks/new')
-      within('form') do 
-        fill_in 'task_title', with: 'task1'
-        fill_in 'task_content', with: '123'
-        fill_in 'task_start_time', with: Time.now
-        fill_in 'task_end_time', with: Time.now
-        select 'high', from: 'task_priority'
-        select 'running', from: 'task_status'
-      end
+      expect(page).to have_current_path(new_task_path)
+    end
+    it 'Successfuly creates a new task' do
+      visit new_task_path
+      fill_data(title: 'task1', content: '123')
       click_button '新增任務'
+      expect_new_task_result
+    end
+
+    def expect_new_task_result
       expect(page).to have_text('新增任務成功')
-      expect(current_path).to have_content(tasks_path)
+      expect(page).to have_current_path(tasks_path)
+      # result = { 'title' => 'task1', 'content' => 'content_new', 'start_time'=> task.start_time, 'end_time'=> task.end_time, 'priority'=>'high', 'status'=>'waiting'}
+      # expect(Task.last.attributes.slice('title', 'content', 'start_time', 'end_time', 'priority', 'status')).to eq(result)
+    end
+
+    it 'Link to edit a task' do
+      visit tasks_path
+      task = Task.find_by(title: task.title)
+      click_link('編輯任務')
+      expect(current_path).to have_content(edit_task_path(task))
     end
 
     it 'Successfuly updates a new task' do
-      visit tasks_path
-      task = Task.find_by(title: 'task1')
-      click_link('編輯任務')
-      expect(current_path).to have_content(edit_task_path(task))
-      within('form') do 
-        fill_in 'task_title', with: 'task2'
-      end
+      fill_data(title:'task1', content:'new_content')
       click_button '更新任務'
+      expect_edit_task_result
+    end
+
+    def expect_edit_task_result
       expect(page).to have_text('更新任務成功')
-      expect(current_path).to have_content(tasks_path)
+      expect(page).to have_current_path(tasks_path)
+    end
+
+    def fill_data(title:, content:)
+      within('form') do
+        fill_in 'task_title', with: title
+        fill_in 'task_content', with: content
+        fill_in 'task_start_time', with: Time.zone.now
+        fill_in 'task_end_time', with: Time.zone.now
+        find_field('task_priority').find('option[selected]').text
+        find_field('task_status').find('option[selected]').text
+      end
+    end
+
+    it 'Link to read a task' do
+      visit tasks_path
+      task_read = Task.find_by(task.title)
+      click_link '查看任務'
+      expect(current_path).to have_content(task_path(task_read))
     end
 
     it 'Successfuly read a task' do
-      visit tasks_path
-      task = Task.find_by(title: 'task1')
-      click_link '查看任務'
-      expect(current_path).to have_content(task_path(task))
       click_link '返回上一頁'
       expect(current_path).to have_content(tasks_path)
     end
