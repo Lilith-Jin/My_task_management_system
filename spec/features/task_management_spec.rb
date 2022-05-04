a00068c31133c62de9377fb686323a2a0beb47bb
Lilith <li6253462@gmail.com>
2022-05-04 15:40:04 +0800

    adjust each context block

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -4,33 +4,33 @@ require 'rails_helper'
 
 RSpec.describe 'task management', type: :feature do
   let!(:task) { FactoryBot.create(:task) }
-
+  subject { page}
   context 'with new page' do
     before do
       visit tasks_path
       click_link(I18n.t('task.action.create'))
     end
 
-    it 'Link to creates a new task' do
-      expect(page).to have_current_path(new_task_path)
-      expect(page).to have_css('form')
-    end
+    describe 'Link to creates a new task' do
+      it { is_expected.to have_current_path(new_task_path) }
+      it { is_expected.to have_css('form') }
 
-    it 'Successfuly creates a new task' do
-      fill_data
-      click_button I18n.t('task.action.create')
-      expect(page).to have_content(task.title)
-      expect(page).to have_text(I18n.t('task.message.success_create'))
-    end
+      it 'Successfuly creates a new task' do
+        fill_data
+        click_button I18n.t('task.action.create')
+        expect(page).to have_content(task.title)
+        expect(page).to have_text(I18n.t('task.message.success_create'))
+      end
 
-    def fill_data
-      within('form') do
-        fill_in 'task_title', with: task.title
-        fill_in 'task_content', with: task.content
-        fill_in 'task_start_time', with: task.start_time
-        fill_in 'task_end_time', with: task.end_time
-        find_field('task_priority').find('option[selected]').text
-        find_field('task_status').find('option[selected]').text
+      def fill_data
+        within('form') do
+          fill_in 'task_title', with: task.title
+          fill_in 'task_content', with: task.content
+          fill_in 'task_start_time', with: task.start_time
+          fill_in 'task_end_time', with: task.end_time
+          find_field('task_priority').find('option[selected]').text
+          find_field('task_status').find('option[selected]').text
+        end
       end
     end
   end
@@ -41,15 +41,14 @@ RSpec.describe 'task management', type: :feature do
       click_link(I18n.t('task.action.update'))
     end
 
-    it 'Link to edit a task' do
-      expect(current_path).to have_content(edit_task_path(task))
-    end
-
-    it 'Successfuly updates a task' do
-      fill_in 'task_title', with: 'new title'
-      click_button(I18n.t('task.action.update'))
-      expect(page).to have_content('new title')
-      expect(page).to have_text(I18n.t('task.message.success_update'))
+    describe 'Link to edit a task' do
+      it { is_expected.to have_current_path(edit_task_path(task)) }
+      it 'Successfuly updates a task' do
+        fill_in 'task_title', with: 'new title'
+        click_button(I18n.t('task.action.update'))
+        expect(page).to have_content('new title')
+        expect(page).to have_text(I18n.t('task.message.success_update'))
+      end
     end
   end
 
@@ -59,26 +58,28 @@ RSpec.describe 'task management', type: :feature do
       click_link I18n.t('task.action.show')
     end
 
-    it 'Link to read a task' do
-      expect(page).to have_current_path(task_path(task))
-      expect(page).to have_text(I18n.t('task.h1.show_task'))
-    end
-
-    it 'Test back to last page button' do
-      click_link I18n.t('task.action.back')
-      expect(page).to have_current_path(tasks_path)
-      expect(page).to have_text(I18n.t('task.h1.index_task'))
+    describe'Link to read a task' do
+      it { is_expected.to have_current_path(task_path(task)) }
+      it { is_expected.to have_text(I18n.t('task.h1.show_task')) }
+      it 'Test back to last page button' do
+        click_link I18n.t('task.action.back')
+        expect(page).to have_text(I18n.t('task.h1.index_task'))
+      end
     end
   end
 
   context 'with delete the task' do
-    it 'Successfuly delete a task', driver: :selenium_chrome, js: true do
+    before do
       visit tasks_path
-      find(:xpath, "//a[@href='/tasks/#{task.id}']", text: I18n.t('task.action.delete')).click
-      accept_alert(text: I18n.t('task.message.confirm_delete'), title: task.title)
-      expect(page).to have_current_path(tasks_path)
-      expect(page).to have_content(I18n.t('task.message.success_delete'))
-      expect(Task.count).to eq(0)
+    end
+
+    describe 'Successfuly delete a task', driver: :selenium_chrome, js: true do
+      it 'Find the delete task'do
+        find(:xpath, "//a[@href='/tasks/#{task.id}']", text: I18n.t('task.action.delete')).click
+        accept_alert(text: I18n.t('task.message.confirm_delete'), title: task.title)
+        expect(page).to have_content(I18n.t('task.message.success_delete'))
+        expect(Task.count).to eq(0)
+      end
     end
   end
 end
