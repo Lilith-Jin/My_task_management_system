ac958914283bb2936dbdb3b5dbd3882f2b17717d
Lilith <li6253462@gmail.com>
2022-05-05 18:07:28 +0800

    add validation on task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  validates :title, presence: true, on: :create
+  validates :content, length: { maximum: 100, too_long: ' %{count} only allow 100 characters ' }, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates_with TaskCustomValidator
+
   enum :priority, %i[high mid low], default: :high
   enum :status, %i[waiting running done], default: :waiting
 end
