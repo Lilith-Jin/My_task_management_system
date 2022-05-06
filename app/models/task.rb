1bb2100b7f73e0b914ea8c1d59ce2f251abf5475
lilith <li6253462@gmail.com>
2022-05-06 19:00:41 +0800

    rewrite the model task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,9 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, length: { maximum: 100, too_long: ' %{count}s only allow 100 characters ' }, presence: true
-  validates :start_time, presence: true
-  validates :end_time, presence: true
-  validates_with TaskStartTimeValidator, TaskEndTimeValidator
+  validates_comparison_of :end_time, greater_than: :start_time
+  validates_comparison_of :end_time, greater_than: Time.zone.today, allow_nil: false
+  validates_comparison_of :start_time, greater_than: Time.zone.today, allow_nil: false
 
   enum :priority, %i[high mid low], default: :high
   enum :status, %i[waiting running done], default: :waiting
