21c95b9acc0a1769852ab412cdf5e19ca7a1e0af
lilith <li6253462@gmail.com>
2022-05-06 22:35:45 +0800

    rewrite task model test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,45 +4,36 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'with task validation' do
-    subject { FactoryBot.build(:task) }
+    subject { FactoryBot.build(:task, add_attrs) }
 
     context 'with task validation' do
-      it 'is valid with valid attributes' do
-        subject.start_time = Time.zone.now
-        subject.end_time = Time.zone.now + 1
-        expect(subject).to be_valid
-      end
+      let(:add_attrs) { {  } }
+      it { is_expected.to be_valid }
+    end
 
-      it 'is not valid without a title' do
-        subject.title = nil
-        expect(subject).not_to be_valid
-      end
+    context 'when without title' do
+      let(:add_attrs) { { title: nil } }
+      it { is_expected.not_to be_valid }
+    end
 
-      it 'is not valid without a content' do
-        subject.content = nil
-        expect(subject).not_to be_valid
-      end
+    context "with content can't be more than 100 characters" do
+      let(:add_attrs) { { content: "a"*101 } }
+      it { is_expected.not_to be_valid }
+    end
 
-      it "with content can't be more than 100 character" do
-        subject.content.size > 100
-        expect(subject).not_to be_valid
-      end
-
-      it 'is not valid without a start_time' do
-        subject.start_time = nil
-        expect(subject).not_to be_valid
-      end
-
-      it 'is not valid without a end_time' do
-        subject.end_time = nil
-        expect(subject).not_to be_valid
-      end
-
-      it "with start_time can't be later than end_time" do
-        subject.start_time = Time.zone.now
-        subject.end_time = Time.zone.now - 1
-        expect(subject).not_to be_valid
-      end
+    context 'when without a start_time' do
+      let(:add_attrs) { { start_time: nil } }
+      it { is_expected.not_to be_valid }
+    end
+      
+    context 'when without a end_time' do
+      let(:add_attrs) { { end_time: nil } }
+      it { is_expected.not_to be_valid }
+    end
+      
+    context "with start_time can't be later than end_time" do
+      let(:add_attrs) { { start_time: Time.at((Time.zone.now + 5.day).to_i) } }
+      it { is_expected.not_to be_valid }
     end
   end
 end
