6ec2bb36251fa8155f937dce610e58fdd225419a
lilith <li6253462@gmail.com>
2022-05-08 16:56:30 +0800

    pass frature test of task order by end_time

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     content { Faker::Lorem.paragraph }
     status { 'waiting' }
     priority { 'high' }
-    start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short) }
-    end_time { Faker::Time.forward(days: 7, format: :short) }
+    start_time { Time.at((Time.zone.now + 1.day).to_i) }
+    end_time { Time.at((Time.zone.now + 5.days).to_i) }
   end
 
   factory :new_task, class: 'Task' do
@@ -15,7 +15,16 @@ FactoryBot.define do
     content { Faker::Lorem.paragraph }
     status { 'waiting' }
     priority { 'high' }
-    start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short) }
-    end_time { Faker::Time.forward(days: 7, format: :short) }
+    start_time { Time.at((Time.zone.now + 1.day).to_i) }
+    end_time { Time.at((Time.zone.now + 3.day).to_i) }
+  end
+
+  factory :last_task, class: 'Task' do
+    title { Faker::Name.name }
+    content { Faker::Lorem.paragraph }
+    status { 'waiting' }
+    priority { 'high' }
+    start_time { Time.at((Time.zone.now + 1.day).to_i) }
+    end_time { Time.at((Time.zone.now + 2.day).to_i) }
   end
 end
