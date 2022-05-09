d518890549d7d6caf41f6359a53f131174161800
lilith <li6253462@gmail.com>
2022-05-09 10:03:39 +0800

    use i18n to display enum name

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@
 class Task < ApplicationRecord
   enum :priority, %i[high mid low], default: :high
   enum :state, %i[waiting running done], default: :waiting
+  # enum state: {waiting:0, running:1, done:2}, default: :waiting
 end
