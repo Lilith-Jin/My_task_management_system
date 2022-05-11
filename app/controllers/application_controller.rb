1021ce48d625cee52854975f1a6c6df2a899b1f4
lilith <li6253462@gmail.com>
2022-05-11 15:13:28 +0800

    remove the comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # before_action :set_ransack_obj
-
-  # def set_ransack_obj
-  #  @q = Task.ransack(params[:q])
-  # end
+ 
 end
