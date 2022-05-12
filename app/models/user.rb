c67d07515b6d056666cd8995bf08d5334ded1b14
lilith <li6253462@gmail.com>
2022-05-12 17:41:37 +0800

    add gem bcrypt to secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
-require 'digest'
-
 class User < ApplicationRecord
-  before_create :encrypt_password
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
-  validates :password, presence: true, length: { in: 6..15 }
+  has_secure_password
 
   enum :role, %i[admin viewer], default: :viewer
 
-  private
-
-  def encrypt_password
-    salted_password = "xyz#{password.reverse}taskmanagement"
-    self.password = Digest::SHA1.hexdigest(salted_password)
-  end
 end
