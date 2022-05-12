6c292efb32fe97c7b5b52460e940b5e4962fecd8
lilith <li6253462@gmail.com>
2022-05-12 16:29:16 +0800

    salted password before create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
+require 'digest'
+
 class User < ApplicationRecord
+
+	before_create :encrypt_password
 	has_many :tasks, dependent: :destroy
+	
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
 	validates :password, presence: true, length: { in: 6..15 }
+
 	enum :role, %i[admin viewer], default: :viewer
+
+	private
+	
+	def encrypt_password
+		salted_password = "xyz#{self.password.reverse}taskmanagement"
+    self.password = Digest::SHA1.hexdigest(salted_password)
+	end
 end
 
