b6bd0ff4415c7671722033bb4955db8cbb232f11
lilith <li6253462@gmail.com>
2022-05-12 15:56:20 +0800

    add validation of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 	has_many :tasks, dependent: :destroy
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
+	validates :password, presence: true, length: { in: 6..15 }
 	enum :role, %i[admin viewer], default: :viewer
 end
 
