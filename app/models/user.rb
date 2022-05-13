c40350741d6d41bc8fc56ada70d51b82c71064b1
lilith <li6253462@gmail.com>
2022-05-13 16:58:06 +0800

    fix user sign_up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
-	
   has_many :tasks, dependent: :destroy
   enum :role, %i[admin viewer], default: :viewer
-
-
-	
 end
