4fd55eb43b1b5f79d3924f9a7202e85a1235a0f7
lilith <li6253462@gmail.com>
2022-05-13 16:46:13 +0800

    fix user sign_in function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@
 
 class ApplicationController < ActionController::Base
     def login(user)
-		session[:session]=user.id
+	  session[:session] = user.id
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,19 +5,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:user][:email])
-    # digest = BCrypt::Password.create(params[:password])
-    if user&.authenticate(params[:user][:password])
-        login(user)
-        redirect_to tasks_path, notice: "登入成功"
+    user = User.find_by(email: user_params[:email])
+  
+    if user&.authenticate(user_params[:password_digest])
+      login(user)
+      redirect_to tasks_path, notice: I18n.t('user.message.success_login')
     else
-        redirect_to :login, alert: "帳號密碼組合不正確，請重新嘗試"
+      redirect_to login_path, notice: I18n.t('user.message.failed_login')
     end
   end
 
-  def logout
+  def destroy
     session[:session] = nil
-    redirect_to root_path, notice: "登出成功，下次見!"
+    redirect_to root_path, notice: '登出成功，下次見!', status: :see_other
   end
-    
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
 end
