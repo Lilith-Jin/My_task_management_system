7c0666a19a04cf341e73064e8a97e8fb3582bd5f
lilith <li6253462@gmail.com>
2022-05-13 23:49:45 +0800

    fix user association

--- db/migrate/20220512021734_add_user_to_tasks.rb
+++ db/migrate/20220512021734_add_user_to_tasks.rb
@@ -1,5 +1,16 @@
 class AddUserToTasks < ActiveRecord::Migration[7.0]
   def change
-    add_reference :tasks, :user, null: false, foreign_key: true
+    add_column :tasks, :user_id, :bigint
+    
+    user = User.find_or_create_by(email: 'lilith@gmail.com') do |u|
+      u.name = 'lilith'
+      u.password = '123456'
+      u.password_confirmation = '123456'
+      u.role = 'admin'
+    end
+
+    Task.update_all(user_id: user.id)
+
+    change_column_null :tasks, :user_id, false
   end
 end
