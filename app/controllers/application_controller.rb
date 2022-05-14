8ade4cd53ff7b4cf5871af9a390ce3fdfd58b6a9
lilith <li6253462@gmail.com>
2022-05-14 23:22:29 +0800

    adjust the indent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@
 class ApplicationController < ActionController::Base
   def login(user)
 	  session[:session] = user.id
-	end
-end
+  end
 
-private
+  private
 
-def current_user
-	@current_user || User.find_by(id: session[:session])
-end
+  def current_user
+    @current_user || User.find_by(id: session[:session])
+  end
 
-def user_sign_in?
-	session[:session].present?
-end
+  def user_sign_in?
+    session[:session].present?
+  end
 
-def authenticate_user
-	redirect_to login_path, notice: I18n.t("user.message.login_first") unless user_sign_in?
+  def authenticate_user
+    redirect_to login_path, notice: I18n.t('user.message.login_first') unless user_sign_in?
+  end
 end
