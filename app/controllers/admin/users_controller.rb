1e64d1588f308c4821989fc7ed66e39c4b659d1a
lilith <li6253462@gmail.com>
2022-05-15 23:27:15 +0800

    add user update function

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,29 @@
 class Admin::UsersController < ApplicationController
+  before_action :find_user, only: %i[edit update destroy]
+
   def index
     @users = User.includes(:tasks).where.not(id: current_user.id).page(params[:page]).per(10)
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: I18n.t('user.message.success_update')
+    else
+      render :edit, notice: I18n.t(user.message.failed_update)
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
