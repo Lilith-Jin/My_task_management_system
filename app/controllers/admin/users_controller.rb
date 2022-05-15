973029cac0644a369b788491993a1d95be6f6e87
lilith <li6253462@gmail.com>
2022-05-16 00:42:47 +0800

    resolve rubocop conflic

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,36 +1,36 @@
 # frozen_string_literal: true
 
-class Admin::UsersController < ApplicationController
-  before_action :find_user, only: %i[edit update destroy]
+module Admin
+  class UsersController < ApplicationController
+    before_action :find_user, only: %i[edit update destroy]
 
-  def index
-    @users = User.includes(:tasks).where.not(id: current_user.id).page(params[:page]).per(10)
-  end
-
-  def edit
-
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to admin_users_path, notice: I18n.t('user.message.success_update')
-    else
-      render :edit, notice: I18n.t(user.message.failed_update)
+    def index
+      @users = User.includes(:tasks).where.not(id: current_user.id).page(params[:page]).per(10)
     end
-  end
 
-  def destroy
-    @user.destroy
-    redirect_to admin_users_path, status: :see_other, notice: I18n.t('user.message.success_delete')
-  end
+    def edit; end
 
-  private 
+    def update
+      if @user.update(user_params)
+        redirect_to admin_users_path, notice: I18n.t('user.message.success_update')
+      else
+        render :edit, notice: I18n.t(user.message.failed_update)
+      end
+    end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
-  end
+    def destroy
+      @user.destroy
+      redirect_to admin_users_path, status: :see_other, notice: I18n.t('user.message.success_delete')
+    end
 
-  def find_user
-    @user = User.find(params[:id])
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :tasks, dependent: :destroy
   has_secure_password
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   has_many :tasks, dependent: :destroy
   enum :role, %i[admin viewer], default: :viewer
 end
