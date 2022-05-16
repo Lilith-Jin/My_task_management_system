78ac433afaa6c55b546e6a410433fc49a53b2d93
lilith <li6253462@gmail.com>
2022-05-16 11:03:41 +0800

    add method to check user role for managing user list

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,7 @@
 module Admin
   class UsersController < ApplicationController
     before_action :find_user, only: %i[edit update destroy]
+    before_action :check_role, only: %i[index edit update destroy]
 
     def index
       @users = User.includes(:tasks).where.not(id: current_user.id).page(params[:page]).per(10)
@@ -31,6 +32,12 @@ module Admin
 
     def find_user
       @user = User.find(params[:id])
+    end
+
+    def check_role
+      unless current_user.role == "admin"
+        redirect_to root_path, notice: I18n.t('user.message.role_admin')
+      end
     end
   end
 end
