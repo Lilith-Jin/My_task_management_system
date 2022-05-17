cf489a523570bbcfa0e0b068305a60e86455a102
lilith <li6253462@gmail.com>
2022-05-17 11:46:34 +0800

    rewrite the scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   has_many :tasks, dependent: :destroy
   enum :role, %i[admin viewer], default: :viewer
-  scope :with_admin_last_one, -> { where(role: 'admin').count == 1 }
+  scope :with_admin_last_one, -> { where(role: 'admin') }
   before_destroy :destroy_admin
 
   private
 
   def destroy_admin
-    return unless admin? && User.with_admin_last_one
+    return unless admin? && User.with_admin_last_one.count == 1
 
     errors.add(:role, message: '至少要存在一位管理員')
     throw(:abort)
