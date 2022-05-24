7fc1b150f1954fcea92c4112798c8c92d6eff94b
lilith <li6253462@gmail.com>
2022-05-24 18:55:40 +0800

    add depedent on tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
-  has_many :task_tags
+  has_many :task_tags, dependent: :destroy
   has_many :tasks, through: :task_tags
   validates :name, presence: true
 end
