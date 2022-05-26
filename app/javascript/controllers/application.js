13eff32918ecf62ef40d0c42a24c266a400cebcc
lilith <li6253462@gmail.com>
2022-05-26 16:13:52 +0800

    remove the comment

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,4 @@
 import { Application } from "@hotwired/stimulus"
-// import jQuery from "jquery"
-// window.jQuery = jQuery // <- "select2" will check this
-// window.$ = jQuery
-
 const application = Application.start()
 
 // Configure Stimulus development experience
