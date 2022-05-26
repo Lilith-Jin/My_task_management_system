ad3ea396e81c8abcc100024c0c8116c46ff938d3
lilith <li6253462@gmail.com>
2022-05-26 12:16:01 +0800

    remove the subject

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,15 +4,11 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   describe 'relation of task' do
-    subject { build(:tag) }
-
     it { is_expected.to have_many(:task_tags).class_name('TaskTag') }
     it { is_expected.to have_many(:tasks).class_name('Task') }
   end
 
   describe 'validation' do
-    subject { build(:tag) }
-
     it { is_expected.to validate_presence_of(:name) }
   end
 end
--- spec/models/task_tag_spec.rb
+++ spec/models/task_tag_spec.rb
@@ -4,8 +4,6 @@ require 'rails_helper'
 
 RSpec.describe TaskTag, type: :model do
   describe 'relation of task' do
-    subject { build(:task_tag) }
-
     it { is_expected.to belong_to(:tag).class_name('Tag') }
     it { is_expected.to belong_to(:task).class_name('Task') }
   end
