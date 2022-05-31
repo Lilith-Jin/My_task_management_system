09a85fd8ceb13a6275604c73fa2fc34011715faf
lilith <li6253462@gmail.com>
2022-05-31 17:12:45 +0800

    seperated create user page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,10 +3,23 @@
 module Admin
   class UsersController < ApplicationController
     before_action :find_user, only: %i[edit update destroy]
-    before_action :check_role, only: %i[index edit update destroy]
+    before_action :check_role
 
     def index
       @users = User.includes(:tasks).page(params[:page]).per(10)
+    end
+
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_users_path, notice: I18n.t('user.message.success_register')
+      else
+        render :new
+      end
     end
 
     def edit; end
